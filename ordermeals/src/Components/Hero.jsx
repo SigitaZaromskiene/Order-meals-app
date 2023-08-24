d1e4409eb8679522f388c3e65a853f728538e285
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-08-24 17:00:23 +0300

    Hero sec

--- ordermeals/src/Components/Hero.jsx
+++ ordermeals/src/Components/Hero.jsx
@@ -1,9 +1,17 @@
-import { food } from "../Assets/img/meals.jpg";
-
 function Hero() {
   return (
-    <div>
-      <img src={food} alt="Tablr full of meals" />
+    <div className="hero">
+      <div className="hero__table">
+        <h4>Delicious Food, Delivered To You</h4>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt
+          nesciunt corporis officiis ipsam odit adipisci fugiat alias distinctio
+          enim! Maxime nostrum debitis dignissimos animi aliquam. Laudantium
+          iste, unde fugiat fugit eaque architecto. Ipsum debitis soluta iusto
+          error, officiis amet sint quas consequatur eum aliquam asperiores ea.
+          Reiciendis molestiae sunt molestias.
+        </p>
+      </div>
     </div>
   );
 }
