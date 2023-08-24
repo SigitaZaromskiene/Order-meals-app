40e6985815f3f408ac20e1401e41a6180cf5d562
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-08-24 16:22:37 +0300

    to hero

--- ordermeals/src/App.jsx
+++ ordermeals/src/App.jsx
@@ -1,10 +1,12 @@
 import "./Assets/App.scss";
+import Hero from "./Components/Hero";
 import Nav from "./Components/Nav";
 
 function App() {
   return (
     <div className="app-container">
       <Nav></Nav>
+      <Hero></Hero>
     </div>
   );
 }
---
+++ ordermeals/src/Components/Hero.jsx
@@ -0,0 +1,11 @@
+import { food } from "../Assets/img/meals.jpg";
+
+function Hero() {
+  return (
+    <div>
+      <img src={food} alt="Tablr full of meals" />
+    </div>
+  );
+}
+
+export default Hero;
